628c2415ae9aa19a4f9a1927a0352c584a6c354c
MadForCoding <hnishad020@gmail.com>
2021-02-15 06:02:57 +0530

    Create Arithmetic.php

---
+++ Arithmetic.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Arithmetic Operator</title>
+</head>
+<body>
+   
+   <?php
+    
+    $num1 = 2;
+    $num2 = 2;
+//    $num3 = $num1 + $num2;
+    
+//    $num3 = $num1 - $num2;
+    
+//     $num3 = $num1 * $num2;
+    
+     $num3 = $num1 / $num2;
+    
+    echo $num3;
+    
+    ?>
+    
+</body>
+</html>
